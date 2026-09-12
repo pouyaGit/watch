898c680578ec42e4f2619f236f7cb1d1daf934c1
pouyaGit <pouya.behnia@gmail.com>
2026-09-12 11:17:39 +0330

    chore(db): make mongo host configurable

--- database/db.py
+++ database/db.py
@@ -32,14 +32,14 @@ def get_domain_name(url):
 
 # Connect to MongoDB
 # connect(db='watch', host='mongodb://127.0.0.1:27017/watch')
-connect(
-    db='watch',
-    host='mongodb://pouya:YourStrongPassword123@127.0.0.1:27017/watch?authSource=admin'
-)
 # connect(
 #     db='watch',
-#     host='mongodb://pouya:YourStrongPassword123@178.83.45.76:27017/?authSource=admin'
+#     host='mongodb://pouya:YourStrongPassword123@127.0.0.1:27017/watch?authSource=admin'
 # )
+connect(
+    db='watch',
+    host='mongodb://pouya:YourStrongPassword123@35.202.201.30:27017/?authSource=admin'
+)
 
 # Define the Programs model
 class Programs(Document):
